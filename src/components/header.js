5058b5083144b3146b61090425949c332ba022f6
Jag <admin@loveforweb.com>
2019-01-25 08:40:53 +0000

    Added nav component

--- src/components/header.js
+++ src/components/header.js
@@ -2,36 +2,17 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Nav from './nav'
+import styles from '../scss/header.module.scss'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header className={styles.header}>
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
-
-      <Nav />
     </div>
-  </div>
+    <Nav />
+  </header>
 )
 
 Header.propTypes = {
